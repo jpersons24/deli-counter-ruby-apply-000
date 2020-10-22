8cf793a07d053533a96301098fa607da9c766d8d
Jakob Persons <jakobep24@gmail.com>
2020-10-22 21:32:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,12 +2,14 @@ katz_deli = []
 
 
 def line(array)
-  array.each do |customer|
-    if array == []
-      p "The line is currently empty."
-    elsif
-      spot = array.index(customer)
-      p "The line is currently #{spot + 1}. #{customer}"
+  if array.length == 0
+    p "The line is currently empty."
+  else
+    message = "The line is currently"
+
+    array.each_with_index do |value, index|
+      message += " #{index.to_i+1}. #{value}"
     end
+    p "#{message}"
   end
 end

ab6c7eea385f7fd81a3a3f7394321c5b149066b8
Jakob Persons <jakobep24@gmail.com>
2020-10-22 21:53:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -27,7 +27,7 @@ end
 
 def now_serving(array)
   if array.length == 0
-    puts "There is nobody waiting to be served."
+    puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{array.shift}."
   end
